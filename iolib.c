aedd4216b2646fded1d5ebdd4f34371e8f1f7b31
Yenting Cho <antonycho@Yentings-Laptop-9.local>
2025-04-19 13:04:18 -0500

    add pseudo code for iolib.c read, write

--- iolib.c
+++ iolib.c
@@ -3,13 +3,30 @@
 // #include <comp421/filesystem.h>
 // #include <comp421/iolib.h>
 #include "iolib.h"
-#include "ysf.h"
+#include "yfs.h"
 
 short cur_dir_idx = ROOTINODE;
 /* 
     create message buffer
     send it to ysf process
 */
+
+int Read(int fd, void *buf, int size) {
+    // used fd find index node, current position
+
+    // inode, current position, buf, size
+
+    // update current position
+    
+}
+
+int Write(int fd, void *buf, int size) {
+    // used fd find index node, current position
+
+    // inode, current position, buf, size
+
+    // update current position
+}
 
 int SymLink(char *oldname, char *newname) {
     // oldname, oldsize, newname, newsize, current directory
